fc30aff933aaa9daafc0522c0b98bfbdf54c905f
LifeFeeling <113740589+LifeFeeling@users.noreply.github.com>
2023-12-24 00:22:25 +0800

    pull

--- src/main/java/com/wzx/git/GitTest.java
+++ src/main/java/com/wzx/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hotfix hello git4!");
         System.out.println("master hello git1!");
         System.out.println("push");
+        System.out.println("pull");
 
     }
 }
